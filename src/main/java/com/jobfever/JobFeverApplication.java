9312dc924b5fbb8035a441cfdd70868b18d83ef9
stelmaszczykadrian <Tooyota12345@>
2023-03-07 10:15:28 +0100

    Initial repo

---
+++ src/main/java/com/jobfever/JobFeverApplication.java
@@ -0,0 +1,13 @@
+package com.jobfever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobFeverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobFeverApplication.class, args);
+	}
+
+}
