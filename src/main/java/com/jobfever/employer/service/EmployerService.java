a0a09f5ffc375be4478e2c8bc67c8a66e62f8b32
reizergrzegorz <reizergrzegorz@gmail.com>
2023-03-09 17:33:03 +0100

    Feature: Rest_Api_Endpoints_Main

--- src/main/java/com/jobfever/employer/service/EmployerService.java
+++ src/main/java/com/jobfever/employer/service/EmployerService.java
@@ -1,5 +1,11 @@
 package com.jobfever.employer.service;
 
+import com.jobfever.candidate.model.Candidate;
+import com.jobfever.candidate.repository.CandidateRepository;
+import com.jobfever.employer.model.Employer;
+import com.jobfever.employer.repository.EmployerRepository;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public class EmployerService {
     public String getEmployerById(int employerId) {
         return "";
@@ -14,4 +20,12 @@ public class EmployerService {
 
     public void editEmployerData(int employerId) {
     }
+
+    public Employer addEmployer(Employer employer) {
+        EmployerRepository.employerList.add(employer);
+        return employer;
+    }
+
+
+
 }
