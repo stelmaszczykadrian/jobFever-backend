793fc087d93de6059436f74f641224d483e33d15
stelmaszczykadrian <Tooyota12345@>
2023-03-21 13:36:45 +0100

    Refactor :  Refactored job service for improved performance

--- src/main/java/com/jobfever/controller/MainController.java
+++ src/main/java/com/jobfever/controller/MainController.java
@@ -13,51 +13,51 @@ import java.util.List;
 @RestController
 public class MainController {
 
-    private CandidateService candidateService;
-    private JobService jobService;
-    private EmployerService employerService;
-
-    public MainController(CandidateService candidateService, JobService jobService, EmployerService employerService) {
-        this.candidateService = candidateService;
-        this.jobService = jobService;
-        this.employerService = employerService;
-    }
-    @GetMapping("/")
-    public String welcome(){
-        return "Welcome on JobFever site";
-    }
-
-    @GetMapping("/jobs")
-    public List<Job> jobList(){
-        return jobService.getJobList();
-    }
-
-    @PostMapping("/register-candidate")
-    public Candidate addCandidate(@RequestBody Candidate candidate){
-        return candidateService.addCandidate(candidate);
-    }
-
-    @PostMapping("/register-employer")
-    public Employer addEmployer(@RequestBody Employer employer) {
-        return employerService.addEmployer(employer);
-    }
-    @GetMapping("/login")
-    public String showLoginPage() {
-        return "login";
-    }
-    @PostMapping("/login")
-    public String submitLoginForm(@RequestParam("username") String username, @RequestParam("password") String password) throws Exception {
-        if (username.equals("myuser") && password.equals("mypassword")) {
-            return "redirect:/jobs";
-        } else {
-            throw new Exception("Wrong user or password");
-        }
-    }
-
-    @GetMapping("/jobs/{job_id}")
-    public Job getJob(@PathVariable("job_id") int jobId){
-        return jobService.getJobById(jobId);
-    }
+//    private CandidateService candidateService;
+//    private JobService jobService;
+//    private EmployerService employerService;
+//
+//    public MainController(CandidateService candidateService, JobService jobService, EmployerService employerService) {
+//        this.candidateService = candidateService;
+//        this.jobService = jobService;
+//        this.employerService = employerService;
+//    }
+//    @GetMapping("/")
+//    public String welcome(){
+//        return "Welcome on JobFever site";
+//    }
+//
+//    @GetMapping("/jobs")
+//    public List<Job> jobList(){
+//        return jobService.getJobList();
+//    }
+//
+//    @PostMapping("/register-candidate")
+//    public Candidate addCandidate(@RequestBody Candidate candidate){
+//        return candidateService.addCandidate(candidate);
+//    }
+//
+//    @PostMapping("/register-employer")
+//    public Employer addEmployer(@RequestBody Employer employer) {
+//        return employerService.addEmployer(employer);
+//    }
+//    @GetMapping("/login")
+//    public String showLoginPage() {
+//        return "login";
+//    }
+//    @PostMapping("/login")
+//    public String submitLoginForm(@RequestParam("username") String username, @RequestParam("password") String password) throws Exception {
+//        if (username.equals("myuser") && password.equals("mypassword")) {
+//            return "redirect:/jobs";
+//        } else {
+//            throw new Exception("Wrong user or password");
+//        }
+//    }
+//
+//    @GetMapping("/jobs/{job_id}")
+//    public Job getJob(@PathVariable("job_id") int jobId){
+//        return jobService.getJobById(jobId);
+//    }
 
 
 }
