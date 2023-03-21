efb64a91a95f8b109a8d1fc46ae591d6183dbaf4
stelmaszczykadrian <Tooyota12345@>
2023-03-21 13:30:08 +0100

    Fix :  Refactor Job model

--- src/main/java/com/jobfever/model/Job.java
+++ src/main/java/com/jobfever/model/Job.java
@@ -1,13 +1,33 @@
 package com.jobfever.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "jobs")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Job {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int jobId;
     private String title;
-    private Employer employer;
+    private String description;
+    private String technicalRequirements;
+    private String responsibilities;
+    private String whoWeAreLookingFor;
+    private String benefits;
+    private String city;
+    private String address;
+    private float salary;
+    private String currency;
+    private LocalDateTime postingDate;
+
+//    private Employer employer;
 
 }
