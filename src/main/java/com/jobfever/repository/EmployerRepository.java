7ced81b9ec75503f416bb8f03ece01eef9f4f937
Karolina Mazurczak <karolinamazurczak.dev@gmail.com>
2023-03-21 12:45:09 +0100

    Feature: Use Spring JPA in Employer layer

--- src/main/java/com/jobfever/repository/EmployerRepository.java
+++ src/main/java/com/jobfever/repository/EmployerRepository.java
@@ -1,12 +1,13 @@
 package com.jobfever.repository;
 
 import com.jobfever.model.Employer;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public interface EmployerRepository {
+public interface EmployerRepository extends JpaRepository<Employer, Integer> {
 
     public final static List<Employer> employerList = new ArrayList<>(Arrays.asList(
 //            new Employer(1, "Samsung", "password"),
