27d50b5e20a8fcbab82b3ac2e0880db6029b8195
Karolina Mazurczak <karolinamazurczak.dev@gmail.com>
2023-03-21 13:10:58 +0100

    Feature: Use Spring JPA in Candidate layer

--- src/main/java/com/jobfever/controller/CandidateController.java
+++ src/main/java/com/jobfever/controller/CandidateController.java
@@ -1,45 +1,56 @@
 package com.jobfever.controller;
 
+import com.jobfever.model.Candidate;
 import com.jobfever.service.CandidateService;
 import com.jobfever.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RequestMapping("/api/candidates")
 @RestController
 public class CandidateController {
 
     private JobService jobService;
-//    private CandidateService candidateService;
-//
-//    @Autowired
-//    public CandidateController(JobService jobService, CandidateService candidateService){
-//        this.jobService = jobService;
-//        this.candidateService = candidateService;
-//    }
-//
-//    @GetMapping("/{candidate_id}")
-//    public String getCandidate(@PathVariable("candidate_id") int candidateId){
-//        return candidateService.getCandidateById(candidateId);
-//    }
-//
-//    @PutMapping("/{candidate_id}/edit")
-//    public String editProfile(@PathVariable("candidate_id") int candidateId){
-//        return candidateService.editProfileById(candidateId);
-//    }
-//
-//    @GetMapping("/{candidate_id}/my-jobs")
-//    public String getJobsOffersAppliedFor(@PathVariable("candidate_id") int candidateId){
-//        return candidateService.getJobOffersAppliedForByCandidateId(candidateId);
-//    }
-//
-//    @GetMapping("/{candidate_id}/favourites")
-//    public String getFavouritesJobs(@PathVariable("candidate_id") int candidateId){
-//        return candidateService.getFavouritesJobsByCandidateId(candidateId);
-//    }
-//
-//    @DeleteMapping("/{candidate_id}")
-//    public boolean deleteCandidateById(@PathVariable("candidate_id") int candidateId){
-//        return candidateService.deleteCandidateById(candidateId);
-//    }
+    private CandidateService candidateService;
+
+    @Autowired
+    public CandidateController(JobService jobService, CandidateService candidateService){
+        this.jobService = jobService;
+        this.candidateService = candidateService;
+    }
+
+    @GetMapping("/{candidate_id}")
+    public Optional<Candidate> getCandidate(@PathVariable("candidate_id") int candidateId){
+        return candidateService.getCandidateById(candidateId);
+    }
+
+    @PutMapping("/{candidate_id}/edit")
+    public String editProfile(@PathVariable("candidate_id") int candidateId){
+        return candidateService.editProfileById(candidateId);
+    }
+
+    @GetMapping("/{candidate_id}/my-jobs")
+    public String getJobsOffersAppliedFor(@PathVariable("candidate_id") int candidateId){
+        return candidateService.getJobOffersAppliedForByCandidateId(candidateId);
+    }
+
+    @GetMapping("/{candidate_id}/favourites")
+    public String getFavouritesJobs(@PathVariable("candidate_id") int candidateId){
+        return candidateService.getFavouritesJobsByCandidateId(candidateId);
+    }
+
+    @DeleteMapping("/{candidate_id}")
+    public boolean deleteCandidateById(@PathVariable("candidate_id") int candidateId){
+        return candidateService.deleteCandidateById(candidateId);
+    }
+
+    @PostMapping("/add_candidate")
+    public void addCandidate(
+            @RequestParam("email") String email,
+            @RequestParam("password") String password
+    ) {
+        candidateService.addCandidate(email, password);
+    }
 }
