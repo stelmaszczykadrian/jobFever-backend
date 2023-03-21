2728df138d0abc569a9d176a9cc340b7b396349a
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-03-21 13:32:33 +0100

    Feature :  Added Job Controller

---
+++ src/main/java/com/jobfever/controller/JobController.java
@@ -0,0 +1,54 @@
+package com.jobfever.controller;
+
+import com.jobfever.model.Job;
+import com.jobfever.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequestMapping("/jobs")
+@RestController
+public class JobController {
+
+    private JobService jobService;
+    @Autowired
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
+    @GetMapping
+    public List<Job> getAllJobs(){
+        return jobService.getAllJobsOffer();
+    }
+
+    @PostMapping
+    public void addJobOffer(){
+        jobService.addJobOffer();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Job> getJobOfferById(
+            @PathVariable int id
+    ){
+        return jobService.getJobById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void updateJobOfferById(
+            @PathVariable int id,
+            @RequestBody Job job
+    ){
+        jobService.updateJobOffer(id, job);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public void deleteJobOfferById(
+            @PathVariable int id
+    ){
+        jobService.deleteJobOfferById(id);
+    }
+
+
+}
