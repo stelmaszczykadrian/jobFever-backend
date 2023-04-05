61fe9c34f8a27d966d6a15ab47ed632f6ef8ce80
Karolina Mazurczak <karolinamazurczak.dev@gmail.com>
2023-04-05 12:21:32 +0200

    Feature: Add Candidate education and experience entities

---
+++ src/main/java/com/jobfever/model/CandidateEducation.java
@@ -0,0 +1,26 @@
+package com.jobfever.model;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "candidate_education")
+@Data
+@NoArgsConstructor
+public class CandidateEducation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String school;
+    private String degree;
+    private String fieldOfStudy;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    @Column(length = 2000)
+    private String description;
+    @ManyToOne
+    private Candidate candidate;
+}
+
