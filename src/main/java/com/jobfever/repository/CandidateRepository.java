824bce4cdce1567271f1cbc28fbbca029956a1af
Piotr Petryka <ppwxp19@gmail.com>
2023-04-17 13:46:02 +0200

    First security attempt

--- src/main/java/com/jobfever/repository/CandidateRepository.java
+++ src/main/java/com/jobfever/repository/CandidateRepository.java
@@ -2,9 +2,12 @@ package com.jobfever.repository;
 
 import com.jobfever.model.Candidate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+@Repository
 public interface CandidateRepository extends JpaRepository<Candidate, Integer> {
-    Candidate findCandidateByEmail(String email);
+    Optional<Candidate> findByEmail(String email);
 
     Candidate findByEmailAndPassword(String email, String password);
 }
