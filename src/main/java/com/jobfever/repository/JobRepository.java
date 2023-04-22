af964534b108ddf12f83039e7749fa9133555132
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-04-22 16:18:21 +0200

    Feature: Added pagination and language filtering

--- src/main/java/com/jobfever/repository/JobRepository.java
+++ src/main/java/com/jobfever/repository/JobRepository.java
@@ -1,8 +1,10 @@
 package com.jobfever.repository;
 import com.jobfever.model.Job;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JobRepository extends JpaRepository<Job, Integer> {
-
+    Page<Job> findByTechnicalRequirementsContainingIgnoreCase(String language, PageRequest postingDate);
 }
 
